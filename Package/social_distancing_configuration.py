9e60a6c95c4836fa0d4f780c5be7dec64bedc017
anubhavsiingh <66999413+anubhavsiingh@users.noreply.github.com>
2021-08-02 20:45:41 +0530

    Add files via upload

---
+++ Package/social_distancing_configuration.py
@@ -0,0 +1,13 @@
+# Basic pth to Yolo-dir
+MODEL_PATH = "yolo-coco"
+
+# Min Probability of object
+MIN_CONF = 0.3
+
+#  MINIMUM NON_MAXIMA suppression threshold
+NMS_THRESH = 0.3
+
+USE_GPU = False
+
+# Min Social Distance 
+MIN_DISTANCE = 50
